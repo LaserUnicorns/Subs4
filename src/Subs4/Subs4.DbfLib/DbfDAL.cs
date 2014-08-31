b97bb2b918bec4e79b22b067b92f469cf96fb0c4
LaserUnicorns <wrd.reg@gmail.com>
2014-08-31 20:47:39 +0400

    finishing up first iteration

--- src/Subs4/Subs4.DbfLib/DbfDAL.cs
+++ src/Subs4/Subs4.DbfLib/DbfDAL.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Odbc;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +15,17 @@ namespace Subs4.DbfLib
         private const string CONNECTION_STRING_FMT = "Driver={{Microsoft dBASE Driver (*.dbf)}};DriverID=277;Dbq={0}";
 
         private OdbcConnection _connection;
+        private string _directoryName;
         private string _tableName;
 
-        public void Connect(string directoryName, string tableName)
+        public void Connect(string directoryName)
         {
-            _connection = new OdbcConnection(string.Format(CONNECTION_STRING_FMT, directoryName));
+            _directoryName = directoryName;
+
+            _connection = new OdbcConnection(string.Format(CONNECTION_STRING_FMT, _directoryName));
             _connection.Open();
 
-            _tableName = tableName;
+            CreateTable();
         }
 
         public void Disconnect()
@@ -38,6 +42,18 @@ namespace Subs4.DbfLib
             _connection.Close();
         }
 
+        private void CreateTable()
+        {
+            _tableName = string.Format("{0:yyMM}{1}{2}.DBF", DateTime.Today, "04", "28");
+            var filename = Path.Combine(_directoryName, _tableName);
+
+            using (var fs = File.Create(filename))
+            {
+                var original = Properties.Resources.original;
+                fs.Write(original, 0, original.Length);
+            }
+        }
+
         public void AddPersonBenefits(Person person)
         {
             using (var cmd = _connection.CreateCommand())
@@ -47,6 +63,7 @@ namespace Subs4.DbfLib
                                   " (pfr, fm, im, ot, dtr, indx, nsp, ulc, dom, korp, kvar, komnata, cat_id,  pr, org_id, usluga, summa)" +
                                   " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+                // parameters should follow in strictly this order
                 cmd.Parameters.AddWithValue("@pfr", person.SNILS);
                 cmd.Parameters.AddWithValue("@fm", person.LastName);
                 cmd.Parameters.AddWithValue("@im", person.FirstName);
@@ -59,6 +76,7 @@ namespace Subs4.DbfLib
                 cmd.Parameters.AddWithValue("@korp", person.Address.Building);
                 cmd.Parameters.AddWithValue("@kvar", person.Address.Flat);
                 cmd.Parameters.AddWithValue("@komnata", person.Address.Room.ToString());
+                cmd.Parameters.AddWithValue("@catid", "");
                 cmd.Parameters.AddWithValue("@pr", string.Empty);
                 cmd.Parameters.AddWithValue("@orgid", string.Empty);
 
--- src/Subs4/TestingApp/Program.cs
+++ src/Subs4/TestingApp/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Subs4.CsvReportReaderLib;
+using Subs4.DbfLib;
 using Subs4.ReportLib;
 using Subs4.XmlPersonsLib;
 
@@ -18,13 +17,25 @@ namespace TestingApp
             var filename = @"C:\Users\smb\SkyDrive\atlant\1408.csv";
             var personsFileName = @"C:\Users\smb\SkyDrive\atlant\persons.xml";
             var pdfFileName = @"C:\Users\smb\SkyDrive\atlant\report.pdf";
+            var dbfdb = @"C:\Users\smb\SkyDrive\atlant";
+
             var xmldal = new XmlPersonsDAL();
             var persons = xmldal.GetPersons(personsFileName);
             var reader = new CsvReportReader(persons);
             var report = reader.Load(filename);
 
-            ReportCreator.CreateReport(report, pdfFileName);
-            Process.Start(pdfFileName);
+            //ReportCreator.CreateReport(report, pdfFileName);
+            //Process.Start(pdfFileName);
+
+            using (var dbf = new DbfDAL())
+            {
+                dbf.Connect(dbfdb);
+
+                foreach (var person in report)
+                {
+                    dbf.AddPersonBenefits(person);
+                }
+            }
         }
     }
 }
